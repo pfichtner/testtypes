03e678c376254fb7adaa712c12f2c62184af61f0
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2023-01-11 00:45:06 +0100

    valid method for autoapproval added

--- src/test/java/testtype/type4approvals/PackageSettings.java
+++ src/test/java/testtype/type4approvals/PackageSettings.java
@@ -3,6 +3,7 @@ package testtype.type4approvals;
 public class PackageSettings {
 
 	public static String ApprovalBaseDirectory = "../resources";
-	// public static AutoApproveWhenEmptyReporter UseReporter = new AutoApproveWhenEmptyReporter();
+	// doesn't seem to work!? See options in TestFizzBuzzApprovaltestWithPBT
+//	public static AutoApproveWhenEmptyReporter UseReporter = new AutoApproveWhenEmptyReporter();
 
 }
--- src/test/java/testtype/type4approvals/TestFizzBuzzApprovaltestWithPBT.java
+++ src/test/java/testtype/type4approvals/TestFizzBuzzApprovaltestWithPBT.java
@@ -5,6 +5,7 @@ import static org.approvaltests.Approvals.verify;
 import static org.approvaltests.namer.NamerFactory.withParameters;
 import static testtype.FizzBuzz.fizzBuzz;
 
+import org.approvaltests.core.Options;
 import org.approvaltests.namer.NamedEnvironment;
 
 import net.jqwik.api.ForAll;
@@ -25,8 +26,14 @@ class TestFizzBuzzApprovaltestWithPBT {
 	@Property(seed = FIXED_SEED, shrinking = OFF)
 	void approvePositiveInts(@ForAll @IntRange(min = 1) int number) {
 		try (NamedEnvironment env = withParameters(number)) {
-			verify(fizzBuzz(number));
+			verify(fizzBuzz(number), options());
 		}
+	}
+
+	private Options options() {
+		return new Options()
+//				.withReporter(new AutoApproveWhenEmptyReporter())
+		;
 	}
 
 }
